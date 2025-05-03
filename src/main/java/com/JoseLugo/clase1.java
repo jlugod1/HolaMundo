b648aa0ced112867ea0b2df846d1adca8b738c75
jlugod1 <jlugopachecom@gmail.com>
2025-05-02 20:32:32 -0400

    segundo commit

---
+++ src/main/java/com/JoseLugo/clase1.java
@@ -0,0 +1,50 @@
+package com.JoseLugo;
+
+public class clase1 {
+    private String atributo1;
+    private String atributo2;
+    private String atributo3;
+    private String atributo4;
+
+    public clase1(String atributo4, String atributo3, String atributo2, String atributo1) {
+        this.atributo4 = atributo4;
+        this.atributo3 = atributo3;
+        this.atributo2 = atributo2;
+        this.atributo1 = atributo1;
+    }
+
+    public String getAtributo1() {
+        return atributo1;
+    }
+
+    public String getAtributo2() {
+        return atributo2;
+    }
+
+    public String getAtributo3() {
+        return atributo3;
+    }
+
+    public String getAtributo4() {
+        return atributo4;
+    }
+
+    public void setAtributo2(String atributo2) {
+        this.atributo2 = atributo2;
+    }
+
+    public void setAtributo1(String atributo1) {
+        this.atributo1 = atributo1;
+    }
+
+    public void setAtributo3(String atributo3) {
+        this.atributo3 = atributo3;
+    }
+
+    public void setAtributo4(String atributo4) {
+        this.atributo4 = atributo4;
+    }
+}
+
+
+
